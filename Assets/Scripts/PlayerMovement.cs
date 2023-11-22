988a2582d3d56054a2b736cde3ebfa259f6e46cc
ZagloSs <carlosdiezmendez@gmail.com>
2023-11-22 13:19:56 +0100

    Movimiento de las palas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 20;
+    public float velocidad = 20;
+    private Rigidbody2D rb;
+    private Vector2 direccion;
+    public KeyCode upKey, downKey;
+    
+
+
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+
+        direccion = Vector2.zero;
+
+        if (Input.GetKey(upKey))
+        {
+            direccion = new Vector2(0, 1);
+        }else if(Input.GetKey(downKey)){
+
+            direccion = new Vector2(0, -1);
+        }
         
+
+
+    }
+
+    void FixedUpdate(){
+        //Update que se ejecuta cada más tiempo
+
+        rb.velocity = direccion * velocidad;
+
+
     }
 }
+ 
